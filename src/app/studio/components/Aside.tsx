c995f2c43ab53ed7bca6bba81d4e15fee7fd5463
LugpDev2022 <lugpdev2022@gmail.com>
2024-01-21 20:34:39 -0600

    Make studio aside a server component

--- src/app/studio/components/Aside.tsx
+++ src/app/studio/components/Aside.tsx
@@ -1,20 +1,11 @@
-'use client';
-
 import Image from 'next/image';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 
 import Nav from './Nav';
 import logo from '@/public/studio-logo.png';
 import Searchbox from './Searchbox';
 
 const Aside: React.FC = () => {
-  const pathname = usePathname();
-
-  if (pathname === '/studio/login') {
-    return <></>;
-  }
-
   return (
     <aside className='h-svh w-[240px] fixed items-center bg-white/5 inline-flex flex-col '>
       <header className='pt-5'>
