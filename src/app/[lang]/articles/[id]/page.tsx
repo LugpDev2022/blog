b6bea3bffe46f335c2c8bfe065c228dff548afc5
LugpDev2022 <lugpdev2022@gmail.com>
2024-02-08 11:04:10 -0600

    Update article page to fetch article by id

--- src/app/[lang]/articles/[id]/page.tsx
+++ src/app/[lang]/articles/[id]/page.tsx
@@ -1,50 +1,40 @@
+import { MDXRemote } from 'next-mdx-remote/rsc';
+
+import { formatDate } from '@/src/app/utils';
 import { Locale } from '@/src/types/shared.types';
 
 interface Props {
-  params: { lang: Locale };
+  params: { lang: Locale; id: string };
 }
 
-const ArticlePage: React.FC<Props> = async ({ params: { lang } }) => {
+const fetchArticle = async (id: string) => {
+  const resp = await fetch(
+    `${process.env.CURRENT_DOMAIN}/api/articles/${id}`,
+    // Revalidate each day
+    { next: { revalidate: 86400 } }
+  );
+
+  const data = await resp.json();
+
+  if (resp.status !== 200) {
+    throw new Error(data.message);
+  }
+
+  return data;
+};
+
+const ArticlePage: React.FC<Props> = async ({ params: { lang, id } }) => {
+  const article = await fetchArticle(id);
+
   return (
     <>
-      <h1 className='article-title'>Lorem ipsum dolor sit amet, consectetur</h1>
+      <h1 className='article-title'>{article[lang].title}</h1>
       <h2 className='article-info'>
-        <time>4/dic/2023</time> - Luis Cervantes
+        <time>{formatDate(article.createdAt, lang)}</time> - Luis Cervantes
       </h2>
       <hr />
 
-      <h3 className='article-subtitle'>Subtitle 1</h3>
-      <p className='article-p'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum
-        sapien velit, et semper felis pharetra a. Ut egestas, dui quis
-        ullamcorper hendrerit, mi libero molestie neque, eu sollicitudin erat
-        dui a erat. Maecenas dapibus massa at commodo porta. Etiam ut dolor ut
-        felis sollicitudin consectetur non eget magna. Ut lobortis pellentesque
-        ipsum at convallis. Aenean at massa vitae libero feugiat semper quis sit
-        amet risus. Integer a purus in ex luctus posuere convallis id dui.
-      </p>
-
-      <h3 className='article-subtitle'>Subtitle 2</h3>
-      <p className='article-p'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum
-        sapien velit, et semper felis pharetra a. Ut egestas, dui quis
-        ullamcorper hendrerit, mi libero molestie neque, eu sollicitudin erat
-        dui a erat. Maecenas dapibus massa at commodo porta. Etiam ut dolor ut
-        felis sollicitudin consectetur non eget magna. Ut lobortis pellentesque
-        ipsum at convallis. Aenean at massa vitae libero feugiat semper quis sit
-        amet risus. Integer a purus in ex luctus posuere convallis id dui.
-      </p>
-
-      <h3 className='article-subtitle'>Subtitle 3</h3>
-      <p className='article-p'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum
-        sapien velit, et semper felis pharetra a. Ut egestas, dui quis
-        ullamcorper hendrerit, mi libero molestie neque, eu sollicitudin erat
-        dui a erat. Maecenas dapibus massa at commodo porta. Etiam ut dolor ut
-        felis sollicitudin consectetur non eget magna. Ut lobortis pellentesque
-        ipsum at convallis. Aenean at massa vitae libero feugiat semper quis sit
-        amet risus. Integer a purus in ex luctus posuere convallis id dui.
-      </p>
+      <MDXRemote source={article[lang].content} />
     </>
   );
 };
