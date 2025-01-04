7d8ef3b9c942496399dbc82bb295ea5448376864
LugpDev2022 <lugpdev2022@gmail.com>
2025-01-03 19:07:57 -0600

    feat(tailwind): change cyan 300 color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: {
+          300: "#36DDE8",
+        },
+      },
+    },
   },
   plugins: [],
 };
