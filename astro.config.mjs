dc3d335b7cae822eedd896e9eb28bd3bb2fb3fc7
LugpDev2022 <lugpdev2022@gmail.com>
2025-01-03 21:14:52 -0600

    feat(navbar): style the searchbox)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   server: { port: 3000 },
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
 });
