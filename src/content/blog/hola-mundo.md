4ace2d45f4bb477b95ce8c8891d2cbaf27e73155
LugpDev2022 <lugpdev2022@gmail.com>
2025-01-06 15:48:43 -0600

    feat(articles-page): render all the articles

---
+++ src/content/blog/hola-mundo.md
@@ -0,0 +1,8 @@
+---
+title: "Bienvenido a mi Blog"
+description: "Hola Mundo"
+date: 16/08/2024
+category: "React"
+---
+
+# Hola Mundo
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,22 @@
 import { defineCollection, z } from "astro:content";
 
+const techCategoryValues = [
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next",
+  "DevOps",
+  "Node.js",
+  "Testing",
+  "Astro",
+] as const;
+
 const blog = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
     date: z.string(),
+    category: z.enum(techCategoryValues),
   }),
 });
 
