6c76d1928f38cfc2dd27cd25b662498fa8966c7b
LugpDev2022 <lugpdev2022@gmail.com>
2025-01-10 16:52:36 -0600

    feat: update astro and add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,12 @@ import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   server: { port: 3000 },
   integrations: [tailwind(), icon(), react()],
   output: "server",
+  adapter: vercel(),
 });
